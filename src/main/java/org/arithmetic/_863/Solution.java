9dcc1586c41f70dd1015bcad24ae7b95779785e5
wei.cai <wei.cai@hand-china.com>
2021-07-28 17:32:23 +0800

    feat (算法): 863. 二叉树中所有距离为 K 的结点
    863. 二叉树中所有距离为 K 的结点

---
+++ src/main/java/org/arithmetic/_863/Solution.java
@@ -0,0 +1,105 @@
+package org.arithmetic._863;
+
+import java.util.*;
+
+/**
+ * 863. 二叉树中所有距离为 K 的结点
+ *
+ * @author wei.cai@hand-china.com 2021/7/28
+ */
+public class Solution {
+    Map<Integer, TreeNode> parents = new HashMap<>();
+    List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        // 从 root 出发 DFS，记录每个结点的父结点
+        findParents(root);
+
+        // 从 target 出发 DFS，寻找所有深度为 k 的结点
+        findAns(target, null, 0, k);
+
+        return ans;
+    }
+
+    public void findParents(TreeNode node) {
+        if (node.left != null) {
+            parents.put(node.left.val, node);
+            findParents(node.left);
+        }
+        if (node.right != null) {
+            parents.put(node.right.val, node);
+            findParents(node.right);
+        }
+    }
+
+    public void findAns(TreeNode node, TreeNode from, int depth, int k) {
+        if (node == null) {
+            return;
+        }
+        if (depth == k) {
+            ans.add(node.val);
+            return;
+        }
+        if (node.left != from) {
+            findAns(node.left, node, depth + 1, k);
+        }
+        if (node.right != from) {
+            findAns(node.right, node, depth + 1, k);
+        }
+        if (parents.get(node.val) != from) {
+            findAns(parents.get(node.val), node, depth + 1, k);
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node0 = new TreeNode(0);
+        TreeNode node8 = new TreeNode(8);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node4 = new TreeNode(4);
+
+
+        node3.left = node5;
+        node3.right = node1;
+
+        node5.left = node6;
+        node5.right = node2;
+
+        node2.left = node7;
+        node2.right = node4;
+
+
+        node1.left = node0;
+        node1.right = node8;
+
+
+        final List<Integer> integers = new Solution().distanceK(node3, node5, 2);
+
+        System.out.println(integers);
+
+    }
+
+}
